b4692882febad198f54d66d1c2afdce8db443fe8
vunguqua10 <vunguqua10@gmail.com>
2023-11-21 16:48:37 +0700

    update

--- wp-content/themes/jobscout/inc/template-functions.php
+++ wp-content/themes/jobscout/inc/template-functions.php
@@ -270,7 +270,7 @@ function jobscout_entry_header(){ ?>
             }
 
             if ( is_singular() ) :
-                the_title( '<h1 class="entry-title">', '</h1>' );
+                the_title( '<h1 class="entry-title entry-title-contact">', '</h1>' );
             else :
                 the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
             endif;        
